e6d47b9354353d6a4c940c19a78f00bb111c371b
Crowdbotics <team@crowdbotics.com>
2021-10-06 05:42:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "curly_tree_31148",
+  url: "https://curly_tree_31148.botics.co",
+  api: "https://curly_tree_31148.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
